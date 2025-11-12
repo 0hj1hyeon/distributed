4f9be8929521e2c725a7072d38fa831294ef2f26
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-11-12 13:35:53 +0900

    refactor: UserRepository 초기 비밀번호를 암호화하여 저장

--- src/main/java/com/example/distributed/repository/UserRepository.java
+++ src/main/java/com/example/distributed/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.distributed.repository;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,9 +11,9 @@ public class UserRepository {
 
     private final Map<String, String> userDb = new HashMap<>();
 
-    public UserRepository() {
-        userDb.put("test", "1234");
-        userDb.put("admin", "admin123");
+    public UserRepository(PasswordEncoder passwordEncoder) {
+        userDb.put("test", passwordEncoder.encode("1234"));
+        userDb.put("admin", passwordEncoder.encode("admin123"));
     }
 
     public String findPasswordByUsername(String username) {
