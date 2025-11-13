4297a439eb37468c36bc594af43f6262f9d357a8
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-11-13 23:09:57 +0900

    refactor: 주석 제거

--- src/main/java/com/example/distributed/repository/UserRepository.java
+++ src/main/java/com/example/distributed/repository/UserRepository.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-// 💡 JpaRepository를 상속받아 CRUD 기능을 자동으로 제공받음
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // 💡 사용자 ID로 User 객체 전체를 조회하는 메서드 정의 (JPA 쿼리 메서드)
     Optional<User> findByUsername(String username);
 }
