62ec274604580771a22a2f39b1958a7b87073f7c
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-11-16 22:50:03 +0900

    chore: DTO 직렬화 안정성 확보 및 Lombok 설정 정리

--- src/main/java/com/example/distributed/dto/LoginRequest.java
+++ src/main/java/com/example/distributed/dto/LoginRequest.java
@@ -1,10 +1,13 @@
 package com.example.distributed.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
