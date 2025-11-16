bed747bfa03daee2e509bd02ea0ea51bf05f0c6f
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-11-16 23:13:26 +0900

    refactor: UserService를 ReactiveUserDetailsService로 전환 및 WebFlux 통합

--- src/main/java/com/example/distributed/service/UserService.java
+++ src/main/java/com/example/distributed/service/UserService.java
@@ -2,12 +2,17 @@ package com.example.distributed.service;
 
 import com.example.distributed.domain.User;
 import com.example.distributed.repository.UserRepository;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.distributed.util.JwtTokenProvider;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Service
-public class UserService {
+public class UserService implements ReactiveUserDetailsService {
 
     private final UserRepository userRepository;
     private final JwtTokenProvider tokenProvider;
@@ -25,13 +30,29 @@ public class UserService {
                 .orElse(null);
 
         if (user == null) {
-            return null;
+            throw new RuntimeException("User not found.");
         }
+
         if (passwordEncoder.matches(password, user.getPassword())) {
             System.out.println(username + "님이 로그인에 성공하고 토큰을 발급받았습니다.");
             return tokenProvider.createToken(username);
         } else {
-            return null;
+            throw new RuntimeException("Invalid password.");
         }
+    }
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+
+        return Mono.fromCallable(() -> userRepository.findByUsername(username))
+                .flatMap(optionalUser -> optionalUser.<Mono<UserDetails>>map(user ->
+                                Mono.just(org.springframework.security.core.userdetails.User.builder()
+                                        .username(user.getUsername())
+                                        .password(user.getPassword())
+                                        .roles("USER")
+                                        .build())
+                        )
+                        .orElseGet(() -> Mono.error(new UsernameNotFoundException("User not found: " + username))))
+                .subscribeOn(Schedulers.boundedElastic());
     }
 }
