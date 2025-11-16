16efc90a6e0e9c3f6206b2bdfc4e950544b66538
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-11-16 23:07:08 +0900

    refactor: User 엔티티의 DB 테이블 이름 변경 및 무결성 강화

--- src/main/java/com/example/distributed/domain/User.java
+++ src/main/java/com/example/distributed/domain/User.java
@@ -1,16 +1,12 @@
 package com.example.distributed.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,8 +16,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
     public User(String username, String password) {
