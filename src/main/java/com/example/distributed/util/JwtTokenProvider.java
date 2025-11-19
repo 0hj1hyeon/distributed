e8b77d1a05600e38b40d7d36b1ebc24d32705760
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-11-19 12:13:37 +0900

    refactor: JwtTokenProvider 키 타입 SecretKey로 명시적 변경

--- src/main/java/com/example/distributed/util/JwtTokenProvider.java
+++ src/main/java/com/example/distributed/util/JwtTokenProvider.java
@@ -4,13 +4,15 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
 
-    private final Key key;
+    private final SecretKey key;
 
     private final long tokenValidityInMilliseconds = 1800000;
 
@@ -35,7 +37,7 @@ public class JwtTokenProvider {
     public boolean validateToken(String token) {
         try {
             Jwts.parser()
-                    .verifyWith((javax.crypto.SecretKey) key)
+                    .verifyWith(key)
                     .build()
                     .parseSignedClaims(token);
             return true;
