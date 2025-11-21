106437a9cc3cc42a4cf63e10b320e98b5ec73803
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-11-21 20:44:31 +0900

    chore: 주석 삭제

--- src/main/java/com/example/distributed/controller/FallbackController.java
+++ src/main/java/com/example/distributed/controller/FallbackController.java
@@ -19,7 +19,6 @@ public class FallbackController {
         Map<String, Object> response = new HashMap<>();
         response.put("success", false);
         response.put("message", "현재 시스템 부하로 인해 요청을 처리할 수 없습니다. 잠시 후 다시 시도해 주세요.");
-        // 503 Service Unavailable 상태 코드를 반환하는 것이 가장 적절합니다.
         return new ResponseEntity<>(response, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
